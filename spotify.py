2f9a4bf88344949878603dd80a078623df3f65d0
charliesmart <charles.smart@uconn.edu>
2016-04-28 09:31:04 -0400

    For class presentation

--- spotify.py
+++ spotify.py
@@ -5,13 +5,13 @@ import time
 import sys
 
 reload(sys)
-sys.setdefaultencoding('utf-8')
 
-#Create an empty dictionary to store the artist/URI pairs
-artists = {}
+sys.setdefaultencoding('utf-8')
 
 #Where our final data is stored. Will eventually be a dict of dicts
 relationships = []
+
+artist_list = []
 
 #Keywords to check for duplicate albums
 duplicate_keys = ['deluxe', 'remix', 'clean', 'edited']
@@ -38,74 +38,76 @@ def is_duplicate(name):
         return False
 
 #Read the CSV with artist URIs and store it in a variable
-csv_file = csv.reader(open('uri.csv'))
+csv_file = csv.reader(open('artists.csv'))
+
+for row in csv_file:
+    artist_list.append(row[0])
 
 #Loop through the CSV and parse it into the artists dictionary
-for row in csv_file:
-    artists[row[0]] = row[1]
+for current_artist in artist_list:
 
-#This is where we begin the process of scraping data
-#We start by looping through the artists
-for key in artists:
+    search_name = current_artist.replace(" ", "+")
 
-    uri = artists[key]
+    artist_search = requests.get("https://api.spotify.com/v1/search?q=" + search_name + "&type=artist&market=US&limit=1").text
 
-    #Get all the albums for each artist
-    albums = requests.get('https://api.spotify.com/v1/artists/' + uri + '/albums')
+    search = json.loads(artist_search)
 
-    #Save the albums as a JSON object
-    albums_parsed = json.loads(albums.text)
+    if search['artists']['items']:
 
-    #Keep a list of albums for the artist as we loop through them to avoid duplicates (e.g. delux and clean versions)
-    albums = []
+        uri = search['artists']['items'][0]['uri'][15:]
 
-    #Loop through the albums we got for this artist
-    for album in albums_parsed['items']:
+        #Get all the albums for each artist
+        albums = requests.get('https://api.spotify.com/v1/artists/' + uri + '/albums')
 
-        #print bcolors.WARNING + album['name'] + bcolors.ENDC
+        #Save the albums as a JSON object
+        albums_parsed = json.loads(albums.text)
 
-        if is_duplicate(album['name']):
-            pass
-        else:
-            #Add our album to the list so we can keep track of it
-            albums.append(album['name'].lower())
+        #Keep a list of albums for the artist as we loop through them to avoid duplicates (e.g. delux and clean versions)
+        albums = []
 
-            #Get the URI so we can check the album's songs
-            album_uri = album['uri']
+        #Loop through the albums we got for this artist
+        for album in albums_parsed['items']:
 
-            #Get only the characters of the URI that we need
-            album_uri_part = album_uri[14:]
+            #print bcolors.WARNING + album['name'] + bcolors.ENDC
 
-            #Get the tracks for the album
-            tracks = requests.get('https://api.spotify.com/v1/albums/' + album_uri_part + '/tracks')
-            tracks_parsed = json.loads(tracks.text)
+            if is_duplicate(album['name']):
+                pass
+            else:
+                #Add our album to the list so we can keep track of it
+                albums.append(album['name'].lower())
 
-            #Loop through the tracks
-            for track in tracks_parsed['items']:
+                #Get the URI so we can check the album's songs
+                album_uri = album['uri']
 
-                #Keep a list of the current track
-                track_artists = []
+                #Get only the characters of the URI that we need
+                album_uri_part = album_uri[14:]
 
-                #Loop through the artists on the track and add them to the list, lowercase and minus special characters
-                for artist in track['artists']:
-                    track_artists.append(artist['name'])
+                #Get the tracks for the album
+                tracks = requests.get('https://api.spotify.com/v1/albums/' + album_uri_part + '/tracks')
+                tracks_parsed = json.loads(tracks.text)
 
-                #Remove the album artist from the list, sicne we're only interested in who they're working with
-                if key in track_artists:
-                        track_artists.remove(key)
+                #Loop through the tracks
+                for track in tracks_parsed['items']:
 
-                print key
-                print track_artists
+                    print track['name']
 
-                #Loop through the list of featured artists that we just made
-                for featured in track_artists:
+                    #Keep a list of the current track
+                    track_artists = []
 
-                    #Combine the names of the album artist and artist they're working with to serve as unique dict key
-                    combined = sanitize(key) + sanitize(featured)
+                    #Loop through the artists on the track and add them to the list, lowercase and minus special characters
+                    for artist in track['artists']:
+                        track_artists.append(artist['name'])
 
-                    relationships.append({'Source': key, 'Target': featured, 'Type': 'Undirected'})
+                    #Remove the album artist from the list, sicne we're only interested in who they're working with
+                    if current_artist in track_artists:
+                            track_artists.remove(current_artist)
 
-                    time.sleep(0.05)
+                    print track_artists
+                    #Loop through the list of featured artists that we just made
+                    for featured in track_artists:
+                        if featured in artist_list:
+                            relationships.append({'Source': current_artist, 'Target': featured, 'Type': 'Undirected'})
+                        time.sleep(0.03)
 
 
 csv_keys = relationships[0].keys()
